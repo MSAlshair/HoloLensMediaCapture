8a516dfb6ada2b1462034a53daff2ad41c590455
Mohammed Alshair <MSAlshair@users.noreply.github.com>
2018-10-15 07:20:11 -0500

    1. Added simple UI RawImage to display a placeholder for the image. 2. Created Script, MediaCaptureImplementation.cs to implement MediaCapture to be able to take, save, and display photos in HoloLens. 3. Created Script, MediaCaptureExample.cs, to implement some examples of how to use MediaCaptureImplementation and attached it to unity game object.

---
+++ Assets/Scripts/MediaCaptureExample.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if WINDOWS_UWP
+using UWPExample;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.Storage.Streams;
+#endif
+
+public class MediaCaptureExample : MonoBehaviour {
+    public RawImage image;
+    public Canvas canvas;
+    // Use this for initialization
+    void Start () {
+#if WINDOWS_UWP
+        //Minimum needed to take a photo and SAVE file to pictures library with desired name
+        TakePhotoBasicAndSaveAsync();
+
+        //Minimum neeed to take a photo and SAVE and DISPLAY
+        TakePhotoBasicAndSaveAndDisplay();
+
+        //Below method has examples of other available options to call
+        //TakePhotoMoreOptionsAsync();
+#endif
+    }
+
+#if WINDOWS_UWP
+    public async void TakePhotoBasicAndSaveAsync()
+    {
+        await new MediaCaptureImplementation().TakePhotoAndSavePhotoToPicturesLibraryAsync(
+            "Demo_" + DateTime.Now.ToString("MM_dd_yyyy hh-mm-ss-tt") + ".jpg");
+    }
+
+    public async void TakePhotoBasicAndSaveAndDisplay()
+    {
+        var photoImplementation = new MediaCaptureImplementation();
+        Texture2D texture = await photoImplementation.TakePhotoTexture2DAsync();
+
+        if (image != null)
+        {
+            image.texture = texture;
+            image.SetNativeSize();
+
+            if(canvas != null)
+            {
+                CanvasScaler canvasScaler = canvas.GetComponent<CanvasScaler>();
+
+                if(canvasScaler != null)
+                {
+                    canvasScaler.referenceResolution = new Vector2(texture.width*1.25f, texture.height*1.25f);
+                }
+            }
+        }
+
+        await photoImplementation.SavePhotoToPicturesLibraryAsync();
+    }
+    public async void TakePhotoMoreOptionsAsync()
+    {
+        #region Step 1: Needed (Constructors)
+        //Step 1: Needed
+        //Constructor 1
+        var photoImplementation = new MediaCaptureImplementation();
+
+        //Constructor 2
+        var photoImplementation1 = new MediaCaptureImplementation(processExtraTasksAfterWhenPhotoCaptureFailsMethod);
+        #endregion  Step 1: Needed (Constructors)
+
+        #region Optional Settings Before Taking Photo
+        //Optional Setting: Setting photo resolution to highest available resolution that your device has
+        await photoImplementation.SetPhotoResolutionToAsync(MediaCaptureImplementation.PHOTO_RESOLUTION.HIGHEST);
+
+        //Optional Setting: Setting photo resolution to lowest available resolution that your device has
+        await photoImplementation.SetPhotoResolutionToAsync(MediaCaptureImplementation.PHOTO_RESOLUTION.LOWEST);
+
+        //Optional Setting: Get a list of all available resolutions to choose from
+        var allAvailableResolutions = await photoImplementation.GetPhotoResolutionsAsync();
+        //Optinal Setting: Example of how to select medium resolution after sorting
+        photoImplementation.SetPhotoResolutionTo(allAvailableResolutions.
+            OrderByDescending(item => item.Height * item.Width).ToList()[(allAvailableResolutions.Count / 2)]);
+        #endregion Optional Settings Before Taking Photo
+
+        #region Step 2: Needed [Take Photo]
+        //Step 2: Needed [Use one of the the methods below to take the photo]
+        //Step 2: Method 1
+        await photoImplementation.TakePhotoAsync();
+
+        //Step 2: Method 2
+        byte[] photoBytes = await photoImplementation.TakePhotoBytesAsync();
+
+        //Step 2: Method 3
+        InMemoryRandomAccessStream photoInMemoryRandomAccessStream = 
+            await photoImplementation.TakePhotoInMemoryRandomAccessStreamAsync();
+
+        //Step 2: Method 4
+        Texture2D photoTexture2D =
+            await photoImplementation.TakePhotoTexture2DAsync();
+
+        //Step 2: Method 5 & Save
+        bool photoAndSavoPicturesLibraryWithDefaultName =
+            await photoImplementation.TakePhotoAndSavePhotoToPicturesLibraryAsync();
+
+        //Step 2: Method 6 & Save
+        bool photoAndSavoPicturesLibrary =
+            await photoImplementation.TakePhotoAndSavePhotoToPicturesLibraryAsync(
+                "FileName_" + DateTime.Now.ToString("MM_dd_yyyy hh-mm-ss-tt") + ".jpg");
+
+        //Step 2: Method 7 & Save
+        bool photoAndSavoWhereEverYouWant =
+            await photoImplementation.TakePhotoAndSavePhotoToAsync(
+                await (await StorageLibrary.GetLibraryAsync(Windows.Storage.KnownLibraryId.Pictures)).
+                    SaveFolder.CreateFileAsync("AnyWhere_" + DateTime.Now.ToString("MM_dd_yyyy hh-mm-ss-tt") + ".jpg", 
+                    CreationCollisionOption.GenerateUniqueName));
+        #endregion  Step 2: Needed [Take Photo]
+
+        #region Save Photo
+        //Optional: Save Photo
+        //Method 1
+        bool savoPicturesLibraryWithDefaultName =
+            await photoImplementation.SavePhotoToPicturesLibraryAsync();
+
+        //Method 2
+        bool savoPicturesLibrary =
+            await photoImplementation.SavePhotoToPicturesLibraryAsync(
+                "FileName_" + DateTime.Now.ToString("MM_dd_yyyy hh-mm-ss-tt") +  ".jpg");
+
+        //Method 3
+        bool savoWhereEverYouWant =
+            await photoImplementation.SavePhotoToAsync(
+                await (await StorageLibrary.GetLibraryAsync(Windows.Storage.KnownLibraryId.Pictures)).
+                    SaveFolder.CreateFileAsync("AnyWhere_" + DateTime.Now.ToString("MM_dd_yyyy hh-mm-ss-tt") + ".jpg", 
+                    CreationCollisionOption.GenerateUniqueName));
+        #endregion Save Photo
+
+        #region Optional Get Photo in Different Formats
+        byte[] getPhotoBytes = photoImplementation.GetPhotoBytes();
+
+        InMemoryRandomAccessStream getPhotoInMemoryRandomAccessStream =
+            await photoImplementation.GetPhotoInMemoryRandomAccessStreamAsync();
+
+        Texture2D getPhotoTexture2D =
+            await photoImplementation.GetPhotoTexture2D();
+        #endregion Optional Get Photo in Different Formats
+    }
+
+    private void processExtraTasksAfterWhenPhotoCaptureFailsMethod(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
+    {
+        //Do your process after failing to take photo
+    }
+#endif
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MediaCaptureImplementation.cs
@@ -0,0 +1,332 @@
+﻿#if WINDOWS_UWP
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using UnityEngine;
+using Windows.Devices.Enumeration;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace UWPExample
+{
+    public class MediaCaptureImplementation
+    {
+        public enum PHOTO_RESOLUTION { HIGHEST, LOWEST };
+
+        private bool _isInitialized;
+        private byte[] _photoBytes;
+        
+        private DeviceInformation _selectedDeviceInformation;
+        private ImageEncodingProperties _selectedPhotoResolution;
+        private Action<MediaCapture, MediaCaptureFailedEventArgs> _processExtraTasksAfterWhenPhotoCaptureFailsMethod;
+
+        public MediaCapture MyMediaCapture;
+        
+        public DateTime? Last_Photo_Captured;
+
+        #region Constructors
+        public MediaCaptureImplementation():this(null)
+        {
+        }
+
+        public MediaCaptureImplementation(
+            Action<MediaCapture, MediaCaptureFailedEventArgs> processExtraTasksAfterWhenPhotoCaptureFailsMethod)
+        {
+            _isInitialized = false;
+            _processExtraTasksAfterWhenPhotoCaptureFailsMethod = processExtraTasksAfterWhenPhotoCaptureFailsMethod;
+        }
+        #endregion Constructors
+
+        #region Initialization
+        private async Task InitializeAsync()
+        {
+            MyMediaCapture = new MediaCapture();
+
+            _selectedDeviceInformation = null;
+            _selectedPhotoResolution = null;
+            Last_Photo_Captured = null;
+
+            var mediaCaptureInitializationSettings = await GetSelectedMediaCaptureInitializationSettingsAsync();
+
+            if (mediaCaptureInitializationSettings != null)
+            {
+                await MyMediaCapture.InitializeAsync(mediaCaptureInitializationSettings);
+                MyMediaCapture.Failed += MediaCapture_Failed;
+
+                _isInitialized = true;
+            }
+        }
+
+        #endregion Initialization
+
+        #region Media Capture Events
+        private void MediaCapture_Failed(MediaCapture sender, MediaCaptureFailedEventArgs errorEventArgs)
+        {
+            _processExtraTasksAfterWhenPhotoCaptureFailsMethod?.Invoke(sender, errorEventArgs);
+            _photoBytes = null;
+            System.Diagnostics.Debug.WriteLine("Camera Failed");
+        }
+        #endregion Media Capture Events
+
+        #region Camera Settings
+        public async Task<DeviceInformationCollection> GetCameraDevicesAsync()
+        {
+            return await Windows.Devices.Enumeration.DeviceInformation.FindAllAsync
+                (DeviceClass.VideoCapture);
+        }
+
+        public async Task<int> GetNumberOfCameraDevicesAsync()
+        {
+            return (await GetCameraDevicesAsync()).Count;
+        }
+
+        public async Task<bool> IsCameraAvailableInDevice()
+        {
+            return (await GetNumberOfCameraDevicesAsync()) > 0;
+        }
+
+        public void SelectCameraDevice(DeviceInformation deviceInformation)
+        {
+            if (string.IsNullOrWhiteSpace(deviceInformation.Id))
+            {
+                _selectedDeviceInformation = deviceInformation;
+            }
+        }
+
+        public async Task SelectDefaultCameraDeviceAsync()
+        {
+            if (await IsCameraAvailableInDevice())
+            {
+                var devices = await GetCameraDevicesAsync();
+
+                _selectedDeviceInformation = devices.Count > 0 ? devices.First() : null;
+            }
+        }
+
+        public async Task<MediaCaptureInitializationSettings> GetSelectedMediaCaptureInitializationSettingsAsync()
+        {
+            if (_selectedDeviceInformation == null)
+                await SelectDefaultCameraDeviceAsync();
+
+            if (_selectedDeviceInformation != null)
+            {
+                return new MediaCaptureInitializationSettings()
+                {
+                    PhotoCaptureSource = PhotoCaptureSource.Photo,
+                    MemoryPreference = MediaCaptureMemoryPreference.Auto,
+                    VideoDeviceId = _selectedDeviceInformation.Id,
+                    AudioDeviceId = "",
+                };
+            }
+            else
+            {
+                return null;
+            }
+        }
+        #endregion Camera Settings
+
+        #region Resolution Methods
+        public async Task<IReadOnlyList<ImageEncodingProperties>> GetPhotoResolutionsAsync()
+        {
+            if (_selectedDeviceInformation == null)
+                await SelectDefaultCameraDeviceAsync();
+
+            if (_selectedDeviceInformation != null)
+            {
+                if (!_isInitialized)
+                    await InitializeAsync();
+
+                return MyMediaCapture.VideoDeviceController.GetAvailableMediaStreamProperties(MediaStreamType.Photo)
+                    .Select(item => item as ImageEncodingProperties).Where(item => item != null).ToList();
+            }
+            else
+                return null;
+        }
+
+        public ImageEncodingProperties GetSelectedPhotoResolution()
+        {
+            return _selectedPhotoResolution;
+        }
+
+        public void SetPhotoResolutionTo(ImageEncodingProperties photoResolution)
+        {
+            if (photoResolution != null)
+                _selectedPhotoResolution = photoResolution;
+        }
+
+        public async Task SetPhotoResolutionToAsync(PHOTO_RESOLUTION photoResolution)
+        {
+            var resolutions = await GetPhotoResolutionsAsync();
+
+            if (resolutions != null)
+            {
+                var resolutionsDescending = resolutions.OrderByDescending(Resolution => Resolution.Height * Resolution.Width).ToList();
+
+                if (PHOTO_RESOLUTION.HIGHEST == photoResolution)
+                    _selectedPhotoResolution = resolutionsDescending.First();
+
+                if (PHOTO_RESOLUTION.LOWEST == photoResolution)
+                    _selectedPhotoResolution = resolutionsDescending.Last();
+            }
+            else
+                _selectedPhotoResolution = null;
+        }
+        #endregion Resolution Methods
+
+        #region Take Photo Methods
+        public async Task<byte[]> TakePhotoBytesAsync()
+        {
+            await TakePhotoAsync();
+            return _photoBytes;
+        }
+
+        public async Task<InMemoryRandomAccessStream> TakePhotoInMemoryRandomAccessStreamAsync()
+        {
+            await TakePhotoAsync();
+            return await GetPhotoInMemoryRandomAccessStreamAsync();
+        }
+
+        public async Task<Texture2D> TakePhotoTexture2DAsync()
+        {
+            await TakePhotoAsync();
+
+            return await GetPhotoTexture2D();
+        }
+
+        public async Task<bool> TakePhotoAndSavePhotoToPicturesLibraryAsync()
+        {
+            await TakePhotoAsync();
+            return await SavePhotoToPicturesLibraryAsync(getDefaultFileName());
+        }
+
+        public async Task<bool> TakePhotoAndSavePhotoToPicturesLibraryAsync(string fileNameIncludinFileExtenstion)
+        {
+            await TakePhotoAsync();
+
+            if (string.IsNullOrEmpty(fileNameIncludinFileExtenstion))
+                fileNameIncludinFileExtenstion = getDefaultFileName();
+
+            var picturesLibraryFolder = await StorageLibrary.GetLibraryAsync(Windows.Storage.KnownLibraryId.Pictures);
+            StorageFile file = await picturesLibraryFolder.SaveFolder.CreateFileAsync(fileNameIncludinFileExtenstion,
+                CreationCollisionOption.GenerateUniqueName);
+
+            return await SavePhotoToAsync(file);
+        }
+
+        public async Task<bool> TakePhotoAndSavePhotoToAsync(StorageFile storageFile)
+        {
+            await TakePhotoAsync();
+            return await SavePhotoToAsync(storageFile);
+        }
+
+        public async Task TakePhotoAsync()
+        {
+            if ((await IsCameraAvailableInDevice()))
+            {
+                if (_selectedPhotoResolution == null)
+                    await SetPhotoResolutionToAsync(PHOTO_RESOLUTION.HIGHEST);
+
+                if (!_isInitialized)
+                    await InitializeAsync();
+
+                using (var photoRandomAccessStream = new InMemoryRandomAccessStream())
+                {
+                    Last_Photo_Captured = DateTime.Now;
+                    await MyMediaCapture.CapturePhotoToStreamAsync(_selectedPhotoResolution, photoRandomAccessStream);
+                    _photoBytes = await ConvertFromInMemoryRandomAccessStreamToByteArrayAsync(photoRandomAccessStream);
+                    System.Diagnostics.Debug.WriteLine("Length: " + _photoBytes.Length);
+                }
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("No camera device detected!");
+            }
+        }
+        #endregion Take Photo Methods
+
+        #region Get Photo in Different Formats
+        public static async Task<byte[]> ConvertFromInMemoryRandomAccessStreamToByteArrayAsync(
+            InMemoryRandomAccessStream inMemoryRandomAccessStream)
+        {
+            using (var dataReader = new DataReader(inMemoryRandomAccessStream.GetInputStreamAt(0)))
+            {
+                var bytes = new byte[inMemoryRandomAccessStream.Size];
+                await dataReader.LoadAsync((uint)inMemoryRandomAccessStream.Size);
+                dataReader.ReadBytes(bytes);
+
+                return bytes;
+            }
+        }
+
+        public byte[] GetPhotoBytes()
+        {
+            return _photoBytes;
+        }
+
+        public async Task<InMemoryRandomAccessStream> GetPhotoInMemoryRandomAccessStreamAsync()
+        {
+            if (_photoBytes == null)
+                return null;
+            else
+            {
+                InMemoryRandomAccessStream inMemoryRandomAccessStream = new InMemoryRandomAccessStream();
+                await inMemoryRandomAccessStream.WriteAsync(_photoBytes.AsBuffer());
+                inMemoryRandomAccessStream.Seek(0);
+
+                return inMemoryRandomAccessStream;
+            }
+        }
+
+        public async Task<Texture2D> GetPhotoTexture2D()
+        {
+            var decoder = await BitmapDecoder.CreateAsync(await GetPhotoInMemoryRandomAccessStreamAsync());
+
+            Texture2D texture = new Texture2D((int)decoder.PixelWidth, (int)decoder.PixelHeight);
+            texture.LoadImage(_photoBytes);
+
+            return texture;
+        }
+        #endregion Get Photo in Different Formats
+
+        #region Save Photo
+        public async Task<bool> SavePhotoToPicturesLibraryAsync()
+        {
+            return await SavePhotoToPicturesLibraryAsync(getDefaultFileName());
+        }
+
+        public async Task<bool> SavePhotoToPicturesLibraryAsync(string fileNameIncludinFileExtenstion)
+        {
+            if (string.IsNullOrEmpty(fileNameIncludinFileExtenstion))
+                fileNameIncludinFileExtenstion = getDefaultFileName();
+
+            var picturesLibraryFolder = await StorageLibrary.GetLibraryAsync(Windows.Storage.KnownLibraryId.Pictures);
+            StorageFile file = await picturesLibraryFolder.SaveFolder.CreateFileAsync(fileNameIncludinFileExtenstion, 
+                CreationCollisionOption.GenerateUniqueName);
+
+            return await SavePhotoToAsync(file);
+        }
+
+        public async Task<bool> SavePhotoToAsync(StorageFile storageFile)
+        {
+            if(storageFile != null)
+            {
+                await Windows.Storage.FileIO.WriteBufferAsync(storageFile, _photoBytes.AsBuffer());
+                return true;
+            }
+
+            return false;
+        }
+        #endregion Save Photo
+
+        private string getDefaultFileName()
+        {
+            return "Default_Photo_" + DateTime.Now.ToString("MM_dd_yyyy hh-mm-ss-ttt") + ".jpg";
+        }
+    }
+}
+#endif
